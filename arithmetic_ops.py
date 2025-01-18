4ad3f6860a98491a0f393aac2f86cd1d3d6514d7
Abhishek S. Sikarawar <asikarwar05@gmail.com>
2025-01-18 21:31:29 +0530

    Add files via upload

---
+++ arithmetic_ops.py
@@ -0,0 +1,20 @@
+num = [10,20]
+a,b = num
+
+add = a + b
+print("Addition = ",add)
+
+sub = b - a
+print("Subtraction = ",sub)
+
+mul = a * b
+print("Multiplication = ",mul)
+
+div = b / a
+print("Division = ",div)
+
+mod = b % a
+print("Modulus = ",mod)
+
+flr = b // a
+print("Floor = ",flr)
---
+++ variable.py
@@ -0,0 +1,9 @@
+a = "Hello"
+b = str("I am")
+print(a,b)
+
+# Assign Multiple Values #
+
+x,y,z = "Para","Marco","Gar"
+print(x,y,z)            #Default Adding Spaces;
+print(x+" "+y+" "+z)    #Put the Space;
