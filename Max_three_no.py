672efd932e3b198013b67ccad29f1bf6a750de59
Abhishek S. Sikarawar <asikarwar05@gmail.com>
2025-01-21 23:41:35 +0530

    Add files via upload

---
+++ Max_three_no.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the number = "))
+b = int(input("Enter the number = "))
+c = int(input("Enter the number = "))
+
+if a > b & a > c :
+    print("A is Greather.")
+elif b > a & b > c:
+    print("B is Greather.")
+else:
+    print("C is Greather.")
---
+++ max_two_no.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the number = "))
+b = int(input("Enter the number = "))
+
+if a > b:
+    print("A is Greather")
+else:
+    print("B is Greather.")
