9384b23220004e80cf2e7af85046d49da0accc35
Abhishek S. Sikarawar <asikarwar05@gmail.com>
2025-01-23 23:29:22 +0530

    Add files via upload

---
+++ vow&const.py
@@ -0,0 +1,6 @@
+a = str(input("Enter the Alphabet = "))
+
+if (a == "a" or a == "A" or a == "e" or a == "E" or a == "o" or a == "O" or a == "i" or a == "I" or a == "u" or a == "U"):
+    print("Vowel = ",a)
+else:
+    print("Consonant = ",a)
