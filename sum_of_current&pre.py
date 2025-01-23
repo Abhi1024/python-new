65a75f6047720a1b4d0e098ec10f9fe5ce46774e
Abhishek S. Sikarawar <asikarwar05@gmail.com>
2025-01-24 00:09:35 +0530

    Add files via upload

---
+++ sum_of_current&pre.py
@@ -0,0 +1,8 @@
+sum1 = 0
+user = int(input("Enter the Number = "))
+
+for i in range(sum1,user):
+    sum = sum1 + i
+    print("Current Number = ",i,", Previous Number = ",sum1,", Sum = ",sum)
+
+    sum1 = i
